48b2a1981d437a4ad7f2aff4dc0b8221ad08a548
karim_touati <karim.twetti@gmail.com>
2019-03-20 11:57:34 +0100

    add sign in/home

--- app/src/main/java/com/example/alertaccident/MainActivity.kt
+++ app/src/main/java/com/example/alertaccident/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.alertaccident
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment? ?: return
     }
 }
