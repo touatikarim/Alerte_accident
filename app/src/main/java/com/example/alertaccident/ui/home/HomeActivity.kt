759b02b09a7cb1ff18b2191f2343bab1871d5853
karim_touati <karim.twetti@gmail.com>
2019-04-05 16:00:52 +0100

    add toolbar/drawer

--- app/src/main/java/com/example/alertaccident/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/alertaccident/ui/home/HomeActivity.kt
@@ -2,12 +2,24 @@ package com.example.alertaccident.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.alertaccident.R
+import kotlinx.android.synthetic.main.activity_user.*
+import kotlinx.android.synthetic.main.toolbar.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
+        val navController = findNavController(this,R.id.my_nav_user_fragment)
+        // Set up ActionBar
+        setSupportActionBar(toolbar)
+        toolbar.setTitle("AlertAccident")
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout )
+        // Set up navigation menu
+        navigationView.setupWithNavController(navController)
     }
 }
