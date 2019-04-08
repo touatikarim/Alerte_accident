c1f15045784ce8da4790cb966dc65fc5c9d091fd
karim_touati <karim.twetti@gmail.com>
2019-04-08 14:38:04 +0100

    navigation_component-side/bottom navbar

--- app/src/main/java/com/example/alertaccident/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/alertaccident/ui/home/HomeActivity.kt
@@ -2,8 +2,11 @@ package com.example.alertaccident.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 
 import androidx.navigation.ui.setupWithNavController
 import com.example.alertaccident.R
@@ -17,10 +20,43 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
-       // val navController = Navigation.findNavController(this, R.id.home3)
-        //bottom_navigation.setupWithNavController(navController)
+        setSupportActionBar(toolbar)
+        val navController = Navigation.findNavController(this,R.id.my_nav_user_fragment)
+        setupBottomNavMenu(navController)
+        setupSideNavigationMenu(navController)
+        setupActionBar(navController)
 
     }
 
+    private fun setupBottomNavMenu(navController: NavController){
+        bottom_nav?.let {
+            NavigationUI.setupWithNavController(it,navController)
+        }
+    }
 
+    private fun setupSideNavigationMenu(navController: NavController){
+        nav_view?.let {
+            NavigationUI.setupWithNavController(it,navController)
+        }
+
+    }
+
+   private fun setupActionBar(navController: NavController){
+       NavigationUI.setupActionBarWithNavController(this,navController,drawer_layout)    }
+
+
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        menuInflater.inflate(R.menu.drawer_view,menu)
+//        return true
+//    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val navController=Navigation.findNavController(this,R.id.my_nav_user_fragment)
+        val navigated=NavigationUI.onNavDestinationSelected(item!!,navController)
+        return navigated || super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+         return NavigationUI.navigateUp(Navigation.findNavController(this,R.id.my_nav_user_fragment),drawer_layout)
+    }
 }
