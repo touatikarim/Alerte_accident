ab82382438a9172f254910d62e7b5f8f4e51716f
karim_touati <karim.twetti@gmail.com>
2019-04-10 12:21:48 +0100

    update profile/setting interface

--- app/src/main/java/com/example/alertaccident/ui/settings/Settings.kt
+++ app/src/main/java/com/example/alertaccident/ui/settings/Settings.kt
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
 
 import com.example.alertaccident.R
 
@@ -24,5 +27,18 @@ class Settings : Fragment() {
         return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val options = navOptions {
+            anim {
+                enter = R.anim.slide_in_right
+                exit = R.anim.slide_out_left
+                popEnter = R.anim.slide_in_left
+                popExit = R.anim.slide_out_right
+            }
+        }
+        view.findViewById<TextView>(R.id.id_update)?.setOnClickListener {
+            findNavController().navigate(R.id.action_settings_dest_to_update_profile, null, options)
+        }
+    }
 }
