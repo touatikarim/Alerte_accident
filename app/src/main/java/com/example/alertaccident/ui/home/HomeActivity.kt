f549e275d209ebb2466ec82390d6f03ace25190a
karim_touati <karim.twetti@gmail.com>
2019-06-11 16:24:39 +0100

    get speed values

--- app/src/main/java/com/example/alertaccident/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/alertaccident/ui/home/HomeActivity.kt
@@ -1,24 +1,25 @@
 package com.example.alertaccident.ui.home
 
-import `in`.galaxyofandroid.spinerdialog.SpinnerDialog
+
 import android.app.ActivityOptions
-
+import android.content.Context
 import android.content.Intent
-
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.view.Menu
+import android.util.Log
 import android.view.MenuItem
 import android.view.View
 import android.widget.TextView
 import androidx.core.content.res.ResourcesCompat
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.navigation.get
 import androidx.navigation.ui.NavigationUI
 import com.example.alertaccident.R
-import com.example.alertaccident.helper.Constants
 import com.example.alertaccident.helper.GPSUtils
 import com.example.alertaccident.retrofit.UserManager
 import com.example.alertaccident.ui.Connexion
@@ -38,34 +39,43 @@ import kotlinx.android.synthetic.main.activity_user.*
 
 
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(),SensorEventListener {
+
 
     lateinit var mGoogleApiClient: GoogleApiClient
-    var spinnerdialog: SpinnerDialog?=null
+    lateinit var sensorManager: SensorManager
+    lateinit var accelerometre:Sensor
+    private val TIME_THRESHOLD=75
+    private var mLastTime:Long=0
+    private var mLastX=-1.0f
+    private var mLastY=-1.0f
+    private var  mLastZ=-1.0f
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
         if (!GPSUtils.isLocationEnabled(this))
             GPSUtils.showAlert(this,this)
         setSupportActionBar(toolbar)
-        spinnerdialog= SpinnerDialog(this, Constants.list_of_items,"Select Service",R.style.DialogAnimations_SmileWindow)
         val navController = Navigation.findNavController(this,R.id.my_nav_user_fragment)
         val sp = UserManager.getSharedPref(this)
         val mail=sp.getString("USER_EMAIL","")
         val name=sp.getString("USER_NAME","")
         val token_google=sp.getString("GOOGLE_SIGNED_IN","")
         val id=sp.getString("USER_ID","")
-
         setupBottomNavMenu(navController)
         setupSideNavigationMenu(navController,mail,name)
         setupActionBar(navController)
-
         setsize(logoutfb)
         setsize(logoutgoogle)
+        Log.d("TAG","Initialiazing Sensor Services")
+        sensorManager=getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        accelerometre=sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        Log.d("Start","Registred accelerometer listener")
+        sensorManager.registerListener(this,accelerometre,SensorManager.SENSOR_DELAY_NORMAL)
+
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestEmail()
             .build()
-
         mGoogleApiClient = GoogleApiClient.Builder(this)
             .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
             .build()
@@ -174,6 +184,36 @@ class HomeActivity : AppCompatActivity() {
         drawable!!.setBounds(0,0,width,height)
         textView.setCompoundDrawables(drawable, null, null, null)
 
+
+    }
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+
+    }
+
+    override fun onSensorChanged(p0: SensorEvent?) {
+        //Log.d("Values","onSensorChanged: X=" + p0!!.values[0]+ "Y=" + p0.values[1] + "Z=" + p0.values[2])
+        getAccelerometre(p0)
+
+    }
+
+
+    private fun getAccelerometre(event:SensorEvent?){
+        val now = System.currentTimeMillis()
+        val values=event!!.values
+        if ((now - mLastTime) > TIME_THRESHOLD){
+            val diff=now-mLastTime
+            val speed=Math.abs(values[0] + values[1] + values[2] - mLastX - mLastY - mLastZ) * (diff * 10000)
+            Log.d("SPEED",speed.toString())
+        }
+       mLastTime=now
+         mLastX = values[0]
+         mLastY = values[1]
+         mLastZ = values[2]
+
+
+      //val  accelationSquareRoot = (x*y+y*y+z*z)/(SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH)
+        //val actualTime= event.timestamp
+        //Log.d("accelattion",accelationSquareRoot.toString())
 
     }
 
